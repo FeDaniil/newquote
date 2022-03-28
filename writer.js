f4563ad2c7641102987897fc31bc4d6b42ef135a
Fedorov Daniil <fedaniil@mail.ru>
2022-03-28 18:34:38 +0300

    Simplify writer cli tool

--- writer.js
+++ writer.js
@@ -2,16 +2,16 @@ import bindings from 'bindings';
 const cpp_back = bindings('newquote-cpp-backend');
 import fs from 'fs';
 import fetch from 'node-fetch';
-import * as readline from 'node:readline/promises';
 import {stdin as input, stdout as output} from 'node:process';
 
-const rl = readline.createInterface({ input, output });
-
-const name = await rl.question("Name: ");
-const ava_link = (await rl.question("Avatar link: ")) || "https://vk.com/images/camera_200.png";
-const text = await rl.question("Text:\n");
+const name = "В.И. Ленин";
+// https://dev.vk.com/method/groups.getById
+// https://dev.vk.com/method/users.get
+const ava_link = "https://sun1-57.userapi.com/s/v1/if1/Qw3-M7tKCo0jE7k_YjNJHUypfjIR-VjxZwUl89BT-WrHGHd3l-AHzq1d4fIyTIG4ayrkg_pI.jpg?size=200x200&quality=96&crop=0,0,300,300&ava=1"
+    || "https://vk.com/images/camera_200.png";
+const text =
+`Главная проблема цитат в Интернете заключается в том, что люди сразу верят в их подлинность ☝`;
 const ava_buf = await (await fetch(ava_link)).arrayBuffer();
 
 fs.writeFileSync('test.png', Buffer.from(cpp_back.gen_quote(text, name, ava_buf)));
 console.log("Done, see test.png");
-rl.close();
